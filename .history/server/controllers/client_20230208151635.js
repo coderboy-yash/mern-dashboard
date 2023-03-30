1108f672a47bf7a5b15f58ec3e9cf20ab8272caf
yash <nigamy911@gmail.com>
2023-03-30 14:17:05 +0530

    removed server error

--- .history/server/controllers/client_20230208151635.js
+++ .history/server/controllers/client_20230208151635.js
@@ -27,7 +27,6 @@ export const getProducts = async (req, res) => {
 export const getCustomers = async (req, res) => {
   try {
     const customers = await User.find({ role: "user" }).select("-password");
-    console.log(customers);
     res.status(200).json(customers);
   } catch (error) {
     res.status(404).json({ message: error.message });
